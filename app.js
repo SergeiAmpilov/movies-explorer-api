8675cd30ce9cb3b021b51fa89f5cfb5f76ac0d45
Sergei <sergei.ampilov@gmail.com>
2022-09-11 11:35:45 +0300

    reload

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
+const { login, createUser, logout } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const { checkSignIn, checkSignUp } = require('./middlewares/celebrate');
+const { processError, notFoundRequest } = require('./middlewares/error');
+const corsEnable = require('./functions/cors-enable');
+const { crashTest } = require('./functions/crash-test');
+
+const { PORT = 3001 } = process.env;
+
+const app = express();
+app.use(cookieParser());
+app.use(cors(corsEnable));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(requestLogger);
+
+app.get('/crash-test', crashTest);
+
+app.post('/signin', checkSignIn, login);
+app.post('/signup', checkSignUp, createUser);
+app.post('/signout', logout);
+
+app.use(auth);
+app.use('/users', userRouter);
+app.use('/cards', cardRouter);
+app.use('*', notFoundRequest);
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+});
+
+app.use(errorLogger);
+
+app.use(errors()); // обработчик ошибок celebrate
+app.use(processError);
+
+app.listen(PORT, () => {});
---
+++ functions/cors-enable.js
@@ -0,0 +1,11 @@
+const corsEnable = {
+  origin: [
+    'https://mesto.ampilovs.ru',
+    'http://mesto.ampilovs.ru',
+    'http://localhost:3000',
+    'https://localhost:3000',
+  ],
+  credentials: true,
+};
+
+module.exports = corsEnable;
