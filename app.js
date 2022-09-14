93ef01d34ba3446c0ea425255b472459f1402058
Sergei <sergei.ampilov@gmail.com>
2022-09-14 11:27:00 +0300

    feat: helmet and rate limit

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-
 const router = require('./routes/router');
-
 const { processError } = require('./middlewares/error');
 const corsEnable = require('./utils/cors-enable');
+const { rateLimitConfig } = require('./utils/rate-limit-config');
 
 const { PORT, DB_CONN } = require('./utils/constants');
 
 const app = express();
+app.use(helmet());
+app.use(rateLimit(rateLimitConfig));
 app.use(cookieParser());
 app.use(cors(corsEnable));
 app.use(bodyParser.json());
